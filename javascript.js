9f6b8956c29d80da37b9e06ee386636b05ad0b81
Klaranaslund <naslundklara@gmail.com>
2023-10-18 16:44:58 +0200

    add eventhandlers for operator buttons

--- javascript.js
+++ javascript.js
@@ -1,24 +1,47 @@
-const firstNum = 0;
-const secondNum = 0;
-const operator = '';
+let firstNum = 0;
+let secondNum = 0;
+let operatorIsClicked = false;
+let operator = '';
 const displayTop = document.querySelector('.displayTop');
 const numberArray = {};
 
 function setUpNumbers(){
-
-for (let i = 1; i <= 9; i++) {
-    numberArray[i] = document.querySelector(`#btn-${i}`);
-    numberArray[i].addEventListener('click', () => {
-    display(i);
-     });
+    for (let i = 1; i <= 9; i++) {
+        numberArray[i] = document.querySelector(`#btn-${i}`);
+        numberArray[i].addEventListener('click', () => {
+        display(i);
+        firstNum = i;
+        });
+    }
 }
-}
-
-setUpNumbers();
 
 function display(toBeDisplayed){
     displayTop.textContent = toBeDisplayed;
 }
+
+function setUpOperators(){
+        const addBtn = document.querySelector('#btn-add');
+        addBtn.onclick = () => handleOperatorClick('+');
+
+        const divideBtn = document.querySelector('#btn-div');
+        divideBtn.onclick = () => handleOperatorClick('/');
+
+        const subtractBtn = document.querySelector('#btn-sub')
+        subtractBtn.onclick = () => handleOperatorClick('-');
+        
+        const multiplyBtn = document.querySelector('#btn-mult')
+        multiplyBtn.onclick = () => handleOperatorClick('x');
+}
+
+
+
+function handleOperatorClick(op){
+    display(op);
+    operator = op;
+    operatorIsClicked = true;
+    console.log(op);
+    }
+
 
 
 
@@ -34,6 +57,15 @@ function divide(num1, num2){
 function multiply(num1, num2){
     return num1 * num2;
 }
+
+const equals = document.querySelector('#btn-equals');
+
+equals.addEventListener('click', () => {
+    operate(firstNum,operator,secondNum);
+   // console.log(firstNum+"="+secondNum);
+}
+);
+
 
 function operate(firstNum, operator, secondNum){
     switch(operator){
@@ -51,3 +83,10 @@ function operate(firstNum, operator, secondNum){
     }
    
 }
+
+function runCalculator(){
+    setUpNumbers();
+    setUpOperators();
+}
+
+runCalculator();
